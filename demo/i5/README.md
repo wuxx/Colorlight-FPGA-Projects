de37f7affb865ea0bb06f991d8db7299d2c8d22f
wuxx <wujianhua0924@gmail.com>
2020-10-29 11:39:19 +0800

    update

---
+++ demo/i5/README.md
@@ -0,0 +1,8 @@
+## litex_no_dram
+`$picocom -b 115200 /dev/ttyACM0`  
+`$dapprog litex_no_dram.bit`
+
+## litex_with_dram
+`$picocom -b 38400 /dev/ttyACM0`  
+`$dapprog litex_with_dram.bit`
+
--- tools/ft231xs_init.sh
+++ tools/ft231xs_init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-FTX_PROG=/home/pi/oss/ftx-prog/ftx_prog
+FTX_PROG="sudo /home/pi/oss/ftx-prog/ftx_prog"
 
-${FTX_PROG} --max-bus-power 501
-${FTX_PROG} --manufacturer "FTDI"
-${FTX_PROG} --product "Lattice FPGA LFE5U"
+${FTX_PROG} --max-bus-power 500
+${FTX_PROG} --manufacturer "FER-RADIONA-EMARD"
+${FTX_PROG} --product "ULX3S FPGA 85K v3.0.3"
 ${FTX_PROG} --new-serial-number 120001
 ${FTX_PROG} --cbus 2 TxRxLED
 ${FTX_PROG} --cbus 3 SLEEP
