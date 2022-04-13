3528347ca553165054ce23d1d27ca969ed700f37
wuxx <wujianhua0924@gmail.com>
2022-04-13 10:46:33 +0800

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	new file:   colorlight_i5.svf
    	modified:   ../../factory/i5/factory_flash.sh

--- factory/i5/factory_flash.sh
+++ factory/i5/factory_flash.sh
@@ -12,22 +12,25 @@ succ_count=0
 fail_count=0
 
 #target_image=/home/pi/oss/colorlight/Colorlight-FPGA-Projects/demo/i9/blink.svf
-target_image=/home/pi/oss/colorlight/Colorlight-FPGA-Projects/demo/i5/blink.bit
+#target_image=/home/pi/oss/Colorlight-FPGA-Projects/demo/i5/blink.bit
+target_image=/home/pi/oss/Colorlight-FPGA-Projects/demo/i5/colorlight_i5.svf
 
 while [ 1 ]; do
     ./probe.sh
     if [ $? -eq 0 ]; then
 
         ./unlock.sh
-        #dapprog ${target_image}
+        dapprog ${target_image}
         #ecpdap flash write --freq 5000 ${target_image}
             
         if [ $? -eq 0 ]; then
             succ_count=$(($succ_count+1))  
+            buzzer_succ
             echo -e "\033[32m---------- SUCC [$succ_count] ----------\033[0m"
 
         else
             fail_count=$(($fail_count+1))  
+            buzzer_fail
             echo -e "\033[31m---------- FAIL [$fail_count] ----------\033[0m"
 
         fi
